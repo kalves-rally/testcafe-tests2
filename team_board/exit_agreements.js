e582114418c38ee15e2701be07d09855a96eac55
Karla Alves <karlaayeska12@gmail.com>
2018-09-04 14:42:56 -0600

    role for login

--- team_board/exit_agreements.js
+++ team_board/exit_agreements.js
@@ -1,37 +1,15 @@
 import { Selector, ok, Role } from 'testcafe';
 import { reset } from 'chalk';
-import Login from './page-model';
+import { createRole } from './page-model';
 
  
-// var login = async t => {
-//     await t
-//         .typeText('#j_username', 'test@rallydev.com')
-//         .typeText('#j_password', 'Password')
-//         .click('#login-button')        
-// }
-
-// var login = new Login(async t =>{
-//     await t 
-//         .typeText(login.usernameInput, 'test@rallydev.com')
-//         .typeText(login.passwordInput, 'Password')
-//         .click(login.loginButton)
-// })
-
-const login = new Login('test@rallydev.com', 'Password')
-
 fixture `Team Board`
-    .page(`https://karla0.testn.f4tech.com/#/20330408691d/teamboard`) 
-    .beforeEach(() => new Login())
-    console.log(login)
-
-
-    // beforeEach(async t => {
-    //     await t.login
-    // })
-
-    // .afterEach(async t => {
-    //     await t.eval(() => localStorage.clear());
-    // })
+    .beforeEach(async t => {
+        const role = createRole('test@rallydev.com');
+        await t
+            .useRole(role)
+            .navigateTo('https://karla0.testn.f4tech.com/#/228928406146d/teamboard');
+    })
 
 
     test('Team Board Exit Agreements', async t =>{
--- team_board/page-model.js
+++ team_board/page-model.js
@@ -1,26 +1,16 @@
-import { Selector } from 'testcafe'; 
+import { Role, Selector } from 'testcafe'; 
 
-export default class Login {
-    constructor( username, password){
-        //this.t = t;
-        this.username = username;
-        this.password = password;
-        this.usernameInput = Selector('#j_username')
-        this.passwordInput = Selector('#j_password')
-        this.loginButton = Selector('#login-button')
-        return go()
-        debugger;
-    }
-
-    async go() { 
-        await t
-            .typeText(this.usernameInput, username)
-            .typeText(this.passwordInput, password)
-            .click(this.loginButton)
-    }
-}
-
-
+export const createRole = (username, password = 'Password') =>
+    Role(
+        'https://karla0.testn.f4tech.com',
+        async t => {
+            await t
+                .typeText('#j_username', username)
+                .typeText('#j_password', password)
+                .click('#login-button')
+        },
+        { preserveUrl: true },
+    )
 
 
 // export default class Login {
